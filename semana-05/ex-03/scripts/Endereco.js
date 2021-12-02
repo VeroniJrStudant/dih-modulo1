5f90505a5fb958bf40cc192df60c2dce20f7f4bc
Alecsander Marques <alecsander.marques@arctouch.com>
2021-12-02 20:28:27 -0300

    Consertar o exercicio 3 da semana 5

--- semana-05/ex-03/scripts/Endereco.js
+++ semana-05/ex-03/scripts/Endereco.js
@@ -1,66 +1,69 @@
-/** @format */
+class Endereco {
+  constructor(logradouro, numero, cidade, estado, pais, cep) {
+    const atributes = [logradouro, numero, cidade, estado, pais, cep];
 
-export class Endereco {
-	_logradouro;
-	get logradouro() {
-		return this._logradouro;
-	}
-	set logradouro(value) {
-		this._logradouro = value;
-	}
-	_numero;
-	get _numero() {
-		return this._numero;
-	}
-	set _numero(value) {
-		this._numero = value;
-	}
-	_cidade;
-	get cidade() {
-		return this._cidade;
-	}
-	set cidade(value) {
-		this._cidade = value;
-	}
-	_estado;
-	get estado() {
-		return this._estado;
-	}
-	set estado(value) {
-		this._estado = value;
-	}
-	_pais;
-	get pais() {
-		return this._pais;
-	}
-	set pais(value) {
-		this._pais = value;
-	}
-	_cep;
-	get cep() {
-		return this._cep;
-	}
-	set cep(value) {
-		this._cep = value;
-	}
+    try {
+      for (let i = 0; i < atributes.length; i++) {
+        if (typeof atributes[i] !== "string")
+          throw new Error("Endereço inválido");
+      }
+    } catch (msg) {
+      document.alert(msg.message);
+    }
 
-	constructor(logradouro, numero, cidade, estado, pais, cep) {
-		const atributes = [logradouro, numero, cidade, estado, pais, cep];
+    this._logradouro = logradouro;
+    this._numero = numero;
+    this._cidade = cidade;
+    this._estado = estado;
+    this._pais = pais;
+    this._cep = cep;
+  }
 
-		try {
-			for (let i = 0; i < atributes.length; i++) {
-				if (typeof atributes[i] !== "string")
-					throw new Error("Endereço inválido");
-			}
-		} catch (msg) {
-			document.alert(msg.message);
-		}
+  _logradouro;
+  get logradouro() {
+    return this._logradouro;
+  }
+  set logradouro(value) {
+    this._logradouro = value;
+  }
 
-		this._logradouro = logradouro;
-		this._numero = numero;
-		this._cidade = cidade;
-		this._estado = estado;
-		this._pais = pais;
-		this._cep = cep;
-	}
+  _numero;
+  get numero() {
+    return this._numero;
+  }
+  set numero(value) {
+    this._numero = value;
+  }
+
+  _cidade;
+  get cidade() {
+    return this._cidade;
+  }
+  set cidade(value) {
+    this._cidade = value;
+  }
+
+  _estado;
+  get estado() {
+    return this._estado;
+  }
+  set estado(value) {
+    this._estado = value;
+  }
+
+  _pais;
+  get pais() {
+    return this._pais;
+  }
+  set pais(value) {
+    this._pais = value;
+  }
+
+  _cep;
+  get cep() {
+    return this._cep;
+  }
+  set cep(value) {
+    this._cep = value;
+  }
 }
--- semana-05/ex-03/scripts/app.js
+++ semana-05/ex-03/scripts/app.js
@@ -1,29 +1,25 @@
-/** @format */
-
-import { Endereco } from "./Endereco.js";
-
 const myForm = document.getElementById("myForm");
 myForm.addEventListener("submit", saveAdress);
 
 function saveAdress(event) {
-	event.preventDefault();
+  event.preventDefault();
 
-	const formData = new FormData(myForm);
+  const formData = new FormData(myForm);
 
-	const logradouro = formData.get("logradouro");
-	const numero = formData.get("numero");
-	const cidade = formData.get("cidade");
-	const estado = formData.get("estado");
-	const pais = formData.get("pais");
-	const cep = formData.get("cep");
+  const logradouro = formData.get("logradouro");
+  const numero = formData.get("numero");
+  const cidade = formData.get("cidade");
+  const estado = formData.get("estado");
+  const pais = formData.get("pais");
+  const cep = formData.get("cep");
 
-	var newAdress = new Endereco(logradouro, numero, cidade, estado, pais, cep);
+  const newAdress = new Endereco(logradouro, numero, cidade, estado, pais, cep);
 
-	const p = document.getElementById("paragrafo");
+  const p = document.getElementById("paragrafo");
 
-	p.innerHTML = "<strong>Endereço saldo com sucesso!</strong>";
+  p.innerHTML = "<strong>Endereço saldo com sucesso!</strong>";
 
-	myForm.innerHTML = `Rua: ${newAdress.logradouro}
+  myForm.innerHTML = `Rua: ${newAdress.logradouro}
       Número: ${newAdress.numero}
       Cidade: ${newAdress.cidade}
       Estado: ${newAdress.estado}
