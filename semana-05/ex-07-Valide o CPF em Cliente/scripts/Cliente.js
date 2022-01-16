4d1177fb8b050b8171462d97bc768a7e27646683
VeroniJrStudant <veroni-vieira-jun1@estudante.sesisenai.org.br>
2022-01-16 02:45:51 -0300

    Feito o móduto static do ex-07

--- semana-05/ex-07-Valide o CPF em Cliente/scripts/Cliente.js
+++ semana-05/ex-07-Valide o CPF em Cliente/scripts/Cliente.js
@@ -1,13 +1,6 @@
 /** @format */
 
 export class Cliente {
-	_nome;
-	get nome() {
-		return this._nome;
-	}
-	set nome(value) {
-		this._nome = value;
-	}
 	_cpf;
 	get cpf() {
 		return this._cpf;
@@ -15,29 +8,58 @@ export class Cliente {
 	set cpf(value) {
 		this._cpf = value;
 	}
-	_telefone;
-	get telefone() {
-		return this._telefone;
-	}
-	set telefone(value) {
-		this._telefone = value;
+
+	static testaCPF(strCPF) {
+		let soma;
+		let resto;
+		soma = 0;
+
+		if (strCPF == "00000000000") {
+			return false;
+		}
+
+		for (let i = 1; i <= 9; i++) {
+			soma = soma + parseInt(strCPF.substring(i - 1, i)) * (11 - i);
+		}
+		resto = (soma * 10) % 11;
+
+		if (resto == 10 || resto == 11) {
+			resto = 0;
+		}
+
+		if (resto != parseInt(strCPF.substring(9, 10))) {
+			return false;
+		}
+
+		soma = 0;
+		for (let i = 1; i <= 10; i++) {
+			soma = soma + parseInt(strCPF.substring(i - 1, i)) * (12 - i);
+		}
+		resto = (soma * 10) % 11;
+
+		if (resto == 10 || resto == 11) {
+			resto = 0;
+		}
+
+		if (resto != parseInt(strCPF.substring(10, 11))) {
+			return false;
+		}
+
+		return true;
 	}
 
-	constructor(nome, cpf, telefone) {
-		const adicionaCliente = [nome, cpf, telefone];
+	constructor(cpf) {
+		const adicionaCliente = cpf;
 
 		try {
 			for (let i = 0; i < adicionaCliente.length - 1; i++) {
-				if (typeof adicionaCliente[i] !== "string") {
+				if (typeof adicionaCliente !== "string") {
 					throw new Error("Dados Inválidos");
 				}
 			}
 		} catch (e) {
 			alert(Error);
 		}
-
-		this._nome = nome;
 		this._cpf = cpf;
-		this._telefone = telefone;
 	}
 }
--- semana-05/ex-07-Valide o CPF em Cliente/scripts/app.js
+++ semana-05/ex-07-Valide o CPF em Cliente/scripts/app.js
@@ -1,7 +1,5 @@
 /** @format */
 
-import { Conta } from "./Conta.js";
-import { Endereco } from "./Endereco.js";
 import { Cliente } from "./Cliente.js";
 
 const myForm = document.getElementById("myForm");
@@ -12,50 +10,13 @@ function saveAdress(event) {
 
 	const formData = new FormData(myForm);
 
-	const nome = formData.get("nome");
 	const cpf = formData.get("cpf");
-	const telefone = formData.get("telefone");
 
-	const conta = formData.get("conta");
-	const saldo = formData.get("saldo");
-
-	const logradouro = formData.get("logradouro");
-	const numero = formData.get("numero");
-	const cidade = formData.get("cidade");
-	const estado = formData.get("estado");
-	const pais = formData.get("pais");
-	const cep = formData.get("cep");
-
-	const newAdress = new Endereco(
-		logradouro,
-		numero,
-		cidade,
-		estado,
-		pais,
-		cep
-	);
-	const newClient = new Cliente(nome, cpf, telefone);
-
-	const newConta = new Conta(conta, saldo);
-
-	if (newClient instanceof Cliente) {
-		alert("esta instanciado em Cliente");
-	}
+	const newClient = new Cliente(cpf);
 
 	const p = document.getElementById("paragrafo");
 
 	p.innerHTML = "<strong>Salvo com sucesso!</strong>";
 
-	myForm.innerHTML = `
-      Nome: ${newClient.nome}
-      Cpf: ${newClient.cpf}
-      Telefone: ${newClient.telefone}
-      Conta: ${newConta.conta}
-      Saldo: ${newConta.saldo}
-      Rua: ${newAdress.logradouro}
-      Número: ${newAdress.numero}
-      Cidade: ${newAdress.cidade}
-      Estado: ${newAdress.estado}
-      País: ${newAdress.pais}
-      Cep: ${newAdress.cep}`;
+	myForm.innerHTML = `Cpf: ${newClient.cpf}`;
 }
